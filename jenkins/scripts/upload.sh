60a517987f650ad2dfee21a56bbb556103e954a2
DeepaBC <46997883+DeepaBC@users.noreply.github.com>
2019-04-24 23:08:21 +0530

    Update upload.sh

--- jenkins/scripts/upload.sh
+++ jenkins/scripts/upload.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
 
-  dir('/var/lib/jenkins/workspace/simple-node-js-react-npm-app11'){
+  dir="/var/lib/jenkins/workspace/simple-node-js-react-npm-app11"
 
-        pwd(); //Log current directory
+        
 
             withAWS(region:'us-east-1',credentials:'MBJDG7W2TGY7NMFIDEA, mja8D0KB15yGljoktC8Yuluxd2NMM0madu94nbzi') {
 
                  def identity=awsIdentity();//Log AWS credentials
 
                 // Upload files from working directory 'dist' in your project workspace
-                s3Upload(bucket:"kishore-jenkinsnpm", workingDir:'dist', includePathPattern:'**/*');
+                s3Upload(bucket:"kishore-jenkinsnpm", workingDir:$dir, includePathPattern:'**/*');
             }
-
-         };
        
 	  
